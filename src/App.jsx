ae47a138655c6cc2bfbf6b44d59ee1557f3b97be
Ezz dghedy <ezzmohsend@gmail.com>
2024-02-28 02:24:40 +0200

    inital set up

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <h1 data-theme="dracula" className="text-3xl font-bold underline h-full">
+      Hello world!
+      <button className="btn btn-secondary">Secondary</button>
+    </h1>
+  );
+}
+
+export default App;
